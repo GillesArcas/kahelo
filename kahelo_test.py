f994d0bcc1542bcad158652e9d1d91e5516f7994
dellielo <dellielo03@gmail.com>
2018-02-21 00:36:22 +0100

    Add kahelo version to create config file

--- kahelo_test.py
+++ kahelo_test.py
@@ -44,8 +44,9 @@ def main(verbose = False):
         shell = True
         creationflags = subprocess.CREATE_NEW_CONSOLE
 
+    kahelo.kahelo("-version")
     p = subprocess.Popen('kahelo -server tests/easter.db', shell=shell, creationflags=creationflags)
-    time.sleep(5)
+    # time.sleep(5)
     url = kahelo.server_url() + '/{zoom}/{x}/{y}.jpg'
 
     # make sure tests are done with known configuration
