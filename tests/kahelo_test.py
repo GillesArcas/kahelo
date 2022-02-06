73d9581d43b8b8cb03e143f6531de0c88c1d35f9
Gilles Arcas <gilles.arcas@gmail.com>
2022-02-06 15:13:50 +0100

    Fix function call.

--- tests/kahelo_test.py
+++ tests/kahelo_test.py
@@ -40,7 +40,7 @@ def main():
         shell = True
         creationflags = subprocess.CREATE_NEW_CONSOLE
 
-    p = subprocess.Popen('python kahelo -server easter.db', shell=shell, creationflags=creationflags)
+    p = subprocess.Popen('kahelo -server easter.db', shell=shell, creationflags=creationflags)
     url = kahelo.server_url() + '/{zoom}/{x}/{y}.jpg'
 
     # make sure tests are done with known configuration
